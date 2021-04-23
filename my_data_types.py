11bb01a47d40bad7ad335764b7453de8ad79dde0
colsen83 <chrisolsen@isu.edu>
2021-04-23 14:24:07 -0600

    Create my_data_types.py

---
+++ my_data_types.py
@@ -0,0 +1,7 @@
+my_list=["Astronomy", "Music","Camping"]
+my_tuple=("Chris Stapleton","Jason Isbell", "Jack Johnson")
+my_map={"astronomy":"Moon","music":"Rock", "Camping":"Yellowstone"}
+
+print("My favorite activity is playing " + my_list[1])
+print("My favorite singer is " + my_tuple[2])
+print("My favorite camping spot is" + my_map["Camping"])
